c5501da75fe06a4ca3b7b047b83e381f11112c14
Norman Ananda <nananda@uncc.edu>
2018-10-12 20:24:27 -0400

    Fixed while loop not running numberGame

--- Common_Control.py
+++ Common_Control.py
@@ -1,49 +1,48 @@
-#Roa Locklear
-#Group Members: Addison Curtis, Norman Ananda, Michael Cicoe, Tripp Satterfield
-#Program to Demonstrate control structures in Python
+#Roa Locklear/Norman Ananda
+#Group Members: Addison Curtis, Michael Cicoe, Tripp Satterfield
+#Program to demonstrate control structures in Python
 
-
+#This code runs on Pyton 3!
 
 #Sequential, this is the default interpetration of code execution.
 
 print("This is")
 print("the default")
-print("Order")
-print("of execution.")
-
-
+print("order")
+print("of execution.\n")
 
 #Selection  Python supports the standard conditionals
 
 def numberGame():
+    
+    play_more = 'y'
 
-    question =int(input("Please enter a positive integer: "))
-    type(question)
-#Notice how else if is can be handle  with the key word "elif"
+#demonstration of while l    
+    while(play_more == 'y'):
+
+        question=(input("Please enter a positive integer: "))
+        question=int(question)
+
+#Notice how else if is can be handle with the key word "elif"
 #Also Python relies on correct indentation to group commands together, there are no brackets
 #in terms of control structures, functions, etc.
+    
+        if question<10:
+            print("Why so small?")
+        elif question<500:
+            print("It's okay to think big")
+        elif question>10:
+            print("Okay that is better.")
+        elif question<0:
+            print("No, it was supposed to be positive...")
+        else:
+            print("Okay, things may have gotten out of control...")
+    
+        play_more = input("Wanna guess again? (enter y/n): ")
 
-    if question <10:
-        print("Why so small?")
-    elif question>10:
-        print("Okay that is better.")
-    elif question<500:
-        print("It's okay to think big")
-    elif question<0:
-        print("No, it was supposed to be positive...")
-    else:
-        print("Okay, things may have gotten out of control...")
 
-#There are ways to handle repetition
-    return;
-
+print("Let's play a little game")
 numberGame()
-
-play_more = input("Wanna do that again? (y/n) ")
-
-while (play_more != 'Y' or play_more != 'y' or play_more !='Yes' or play_more != 'yes'):
-    numberGame()
-
 
 #There is also "Short Circuit Evaluation"
 
