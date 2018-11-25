f3b3808d66a36569c4e8b1128797327a0428c367
Addison Curtis <addisongcurtis@gmail.com>
2018-11-24 23:25:29 -0500

    Add class for cards, add more structure for game

--- project/blackjack.py
+++ project/blackjack.py
@@ -7,46 +7,26 @@ pg.font.init()
 display = pg.display.set_mode((1280, 720))
 defaultFont = pg.font.Font(None, 26)
 
+DEFAULT_CARD_SIZE = (100, 153)
+
 # Load in face/back images
-cardImages = cards.loadCardImages()
+cardImages = cards.loadCardImages(DEFAULT_CARD_SIZE)
 
 dealerDeck = cards.shuffle(cards.generateDeckArray())
 dealerHand = []
+dealerHandElements = []
 playerHand = []
+playerHandElements = []
 
-buttons = []
+buttonElements = []
 
-# A rectangular button with text on it
-class Button():
-	def __init__(self, position = (0, 0), color = (255, 255, 255), size = (100, 50), text = "", clickAction = lambda: None):
-		self.position = position
-		self.color = color
-		self.size = size
-		self.text = text
-		self.click = clickAction
-		buttons.append(self)
-
-	def draw(self):
-		drawColor = self.color
-		if self.mouseInButton():
-			# Draw the button slightly differently since it's being hovered over
-			# Be careful not to make the buttons too dark, since it may end up hiding the black text
-			drawColor = tuple(abs(a-20) for a in self.color)
-
-		# * is for unpacking the tuples
-		pg.draw.rect(display, drawColor, (*self.position, *self.size))
-		textSurf, textRect = newTextObject(defaultFont, self.text, (self.position[0]+(self.size[0]/2), self.position[1]+(self.size[1]/2)))
-		display.blit(textSurf, textRect)
-
-	# Returns whether the mouse is currently inside the button area or not
-	def mouseInButton(self):
-		mouseLoc = pg.mouse.get_pos()
-		return self.position[0] < mouseLoc[0] < self.position[0]+self.size[0] and \
-		       self.position[1] < mouseLoc[1] < self.position[1]+self.size[1]
 
 def main():
+	ButtonElement(position = (1170, 10), text = "Hit", clickAction = hit)
+	ButtonElement(position = (1170, 70), text = "Stand", clickAction = lambda: print("Stand button clicked"))
+	ButtonElement(position = (1170, 130), text = "Deal", clickAction = lambda: print("Deal button clicked"))
 
-	Button(position = (200, 200), text = "Testing", clickAction = lambda: print("Test button clicked"))
+	newHand()
 
 	exit = False
 	# Keep going until the player exits or there are no more cards in the deck
@@ -56,21 +36,127 @@ def main():
 				return # Player exited the window
 
 			if event.type == pg.MOUSEBUTTONUP:
-				for button in buttons:
-					if button.mouseInButton():
+				print(pg.mouse.get_pos())
+				for button in buttonElements:
+					if button.mouseInElement():
 						button.click()
-
 		display.fill((60,180,45))
 
-		for button in buttons:
+		for button in buttonElements:
 			button.draw()
+
+		for card in (dealerHandElements + playerHandElements):
+			card.draw()
+
+		if handValue(playerHand) > 21:
+			display.blit(*newTextObject(pg.font.Font(None, 36),
+				                        "You went bust! Click \"Deal\" to start a new round.",
+				                        (640, 360)))
+			pg.display.flip()
+			continue
+
 		# Add other logic here
 
 		pg.display.flip()
 
+
+def handValue(hand):
+	total = 0;
+	for card in hand:
+		total += cards.value(card)
+
+	return total
+
+# Deals the player a new card
+def hit():
+	playerHand.append(*cards.draw(dealerDeck, 1))
+	playerHandElements.append(*genCardElements([playerHand[-1]], 
+		                      startPos = (playerHandElements[-1].position[0]+DEFAULT_CARD_SIZE[0]+ 10, 
+		                      	          720-DEFAULT_CARD_SIZE[1]-10)))
+
+def newHand():
+	global dealerHand, dealerHandElements
+	global playerHand, playerHandElements
+	dealerHand = cards.draw(dealerDeck, 2)
+	
+	dealerHandElements = genCardElements(dealerHand, startPos = (10, 10))
+	dealerHandElements[0].flip()
+	playerHand = cards.draw(dealerDeck, 2)
+	playerHandElements = genCardElements(playerHand, startPos = (10, 720-DEFAULT_CARD_SIZE[1]-10))
+
+
+class Element():
+	def __init__(self, position = (0, 0), size = (0, 0)):
+		self.position = position
+		self.size = size
+
+
+	def draw(self):
+		pass
+
+
+	# Returns whether the mouse is currently inside the button area or not
+	def mouseInElement(self):
+		mouseLoc = pg.mouse.get_pos()
+		return self.position[0] < mouseLoc[0] < self.position[0]+self.size[0] and \
+		       self.position[1] < mouseLoc[1] < self.position[1]+self.size[1]
+
+
+# A rectangular button with text on it
+class ButtonElement(Element):
+	def __init__(self, position = (0, 0), size = (100, 50), color = (255, 255, 255), text = "", textColor = (0, 0, 0), clickAction = lambda: None):
+		super().__init__(position = position, size = size)
+		self.color = color
+		self.text = text
+		self.textColor = textColor
+		self.click = clickAction
+		buttonElements.append(self)
+
+
+	def draw(self):
+		drawColor = self.color
+		if self.mouseInElement():
+			# Draw the button slightly differently since it's being hovered over
+			# Be careful not to make the buttons too dark if you use a black font, since it could hide the text
+			drawColor = tuple(abs(a-20) for a in self.color)
+
+		# * is for unpacking the tuples
+		pg.draw.rect(display, drawColor, (*self.position, *self.size))
+		textSurf, textRect = newTextObject(defaultFont, self.text, 
+			                               (self.position[0]+(self.size[0]/2), self.position[1]+(self.size[1]/2)),
+			                               color = self.textColor)
+		display.blit(textSurf, textRect)
+
+
+# A single image of a card. Use "back" card ID to draw the back of a card, or set "shown" to False
+class CardElement(Element):
+	def __init__(self, position = (0, 0), size = DEFAULT_CARD_SIZE, cardId = "AS", shown = True, clickAction = lambda: None):
+		super().__init__(position = position, size = size)
+		self.cardId = cardId
+		self.click = clickAction
+		self.shown = shown
+
+
+	def draw(self):
+		display.blit(cardImages[self.cardId if self.shown else "back"], self.position)
+
+
+	def flip(self):
+		self.shown = not self.shown
+
+
 def newTextObject(font, text, center, color = (0, 0, 0)):
 	rendered = font.render(text, True, color)
 	return rendered, rendered.get_rect(center=center)
+
+# Returns an array of CardElements that are layed out horizontaly
+def genCardElements(cardIds, startPos = (0, 0)):
+	cardElements = []
+	for i, cardId in zip(range(0, len(cardIds)), cardIds):
+		cardElements.append(CardElement(position = ((DEFAULT_CARD_SIZE[0]+10)*i+startPos[0], startPos[1]), cardId = cardId))
+
+	return cardElements
+
 
 if __name__ == "__main__":
 	main()
--- project/cards.py
+++ project/cards.py
@@ -6,15 +6,22 @@ import random
 # Cards are keyed as a number 2-10 or J/Q/K/A for value, then C/D/H/S for suit
 # So Ace of Spades would be AS
 # 7 of Hearts would be 7H
-def loadCardImages():
+# Size is the (x, y) pixel size
+def loadCardImages(size = (691, 1056)):
 	deckImages = {}
 	baseDir = "card_images/"
 	# Get all card faces
 	for path in glob.iglob(baseDir + "[0-9JQKA]*[CDHS].png"):
-		deckImages[path[len(baseDir):-4]] = pg.image.load(path)
+		deckImages[path[len(baseDir):-4]] = pg.transform.smoothscale(pg.image.load(path), size)
 	# Load in the card back as well
-	deckImages["back"] = pg.image.load(baseDir + "back_grey.png")
+	deckImages["back"] = pg.transform.smoothscale(pg.image.load(baseDir + "back_grey.png"), size)
 	return deckImages
+
+# This is if you want a single new card image of a different size for whatever reason
+# If you want a back image, you must postpend it with the color to match the filename.
+# choices are: blue, green, grey, purple, red, and yellow
+def loadCardImage(cardId, size = (691, 1056)):
+	return pg.transform.smoothscale(pg.image.load("card_images/" + cardId + ".png"), size)
 
 # Returns an array of 52 strings for each card
 # Order is not guaranteed
@@ -30,6 +37,28 @@ def shuffle(deck):
 	shuffledDeck = random.sample(deck, len(deck))
 	return shuffledDeck
 
+# Returns some number of cards from the deck while also removing them
+def draw(deck, numCards):
+	drawnCards = []
+	for i in range(0, numCards):
+		drawnCards.append(deck.pop())
+	return drawnCards
+
+# Returns the numarical value for a card
+# Returns a negative 1 for an ace, since it could be either 1 or 10
+def value(cardId):
+	# 2-10 are just the face value
+	if cardId[:-1].isdigit():
+		return int(cardId[:-1])
+
+	# All royal cards are worth 10
+	return 10 if cardId[0] in ["J", "Q", "K"] else -1
+
+
 # Run the file directly to use this for testing
 if __name__ == '__main__':
-	print(generateDeckArray())
+	values = []
+	cards = generateDeckArray()
+	for card in cards:
+		values.append(value(card))
+	print(values, cards)
